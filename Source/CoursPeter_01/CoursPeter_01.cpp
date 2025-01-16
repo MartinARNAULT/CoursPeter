4a8f0d48afe03c144bb7d9a0daa16c733b66ac07
MartinARNAULT <m.arnault@rubika-edu.ca>
2025-01-16 10:37:07 -0500

    update

---
+++ Source/CoursPeter_01/CoursPeter_01.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CoursPeter_01.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CoursPeter_01, "CoursPeter_01" );
